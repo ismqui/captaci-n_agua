6cce5fdb26564a275f6f022e6627ba90a0130bd7
ismqui <ismqui.git@gmail.com>
2018-01-25 15:39:21 +0100

    add deps and update config

--- lib/captacion_agua/load_data.ex
+++ lib/captacion_agua/load_data.ex
@@ -1,4 +1,7 @@
 defmodule CaptacionAgua.LoadData do
   @remote_file_url "http://www.ine.es/jaxi/files/_px/es/csv_c/t26/p067/p01/serie/l0/01001.csv_c?nocab=1"
-  
+
+  def load_data do
+    Download.get_file(@remote_file_url, "assets/captacion.dat")
+  end
 end
