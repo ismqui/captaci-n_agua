9c3a0749d71080bdb1cb5f5d2877ade2c563ddf2
ismqui <ismqui.git@gmail.com>
2018-01-25 20:05:25 +0100

    function to parse data

--- lib/captacion_agua/load_data.ex
+++ lib/captacion_agua/load_data.ex
@@ -4,4 +4,53 @@ defmodule CaptacionAgua.LoadData do
   def load_data do
     Download.get_file(@remote_file_url, "assets/captacion.dat")
   end
+
+  def parse_data do
+    {:ok, pid} = File.open("assets/captacion.dat", [:read, :utf8])
+    read_titles(pid)
+    years = read_years(pid)
+
+    list_places = for _x <- 1..19, into: [], do: read_data_place(pid, years)
+    notas = IO.read(pid, :line)
+    fuente = IO.read(pid, :line)
+    list_places
+  end
+
+  def read_titles(pid) do
+    _titulo1 = IO.read(pid, :line)
+    _titulo2 = IO.read(pid, :line)
+    _descripcion = IO.read(pid, :line)
+    _unidades = IO.read(pid, :line)
+    _linea = IO.read(pid, :line)
+  end
+
+  def read_years(pid) do
+    load_data(pid)
+  end
+
+  def read_data_place(pid, years) do
+    place = load_place(pid)
+    total = load_data(pid)
+    superficiales = load_data(pid)
+    subterraneas = load_data(pid)
+    desalacion = load_data(pid)
+    %{
+      place: place,
+      years: years,
+      total: total,
+      superficiales: superficiales,
+      subterraneas: subterraneas,
+      desalacion: desalacion
+    }
+  end
+
+  defp load_place(pid) do
+    IO.read(pid, :line) |> String.split(",") |> List.first
+  end
+
+  defp load_data(pid) do
+    [ _h | t ] = IO.read(pid, :line) |> String.split("\n") |> List.first |> String.split(",")
+    count = Enum.count(t)
+    Enum.take(t, count - 1)
+  end
 end
