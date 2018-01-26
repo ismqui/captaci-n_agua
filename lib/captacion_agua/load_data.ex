aa796ef831cd78ad4e9e318f4d39b7e93c778a2d
ismqui <ismqui.git@gmail.com>
2018-01-26 22:12:07 +0100

    add agent

--- lib/captacion_agua/load_data.ex
+++ lib/captacion_agua/load_data.ex
@@ -1,6 +1,17 @@
 defmodule CaptacionAgua.LoadData do
   @remote_file_url "http://www.ine.es/jaxi/files/_px/es/csv_c/t26/p067/p01/serie/l0/01001.csv_c?nocab=1"
 
+
+  def start_link do
+    load_data()
+    list = parse_data()
+    Agent.start_link(fn -> list end, name: __MODULE__)
+  end
+
+  def get_data(place) do
+    Agent.get(__MODULE__, fn list -> Enum.filter(list, fn x -> x.place == place end) end)
+  end
+
   def load_data do
     Download.get_file(@remote_file_url, "assets/captacion.dat")
   end
