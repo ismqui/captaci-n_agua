8faffed7bc2bd6f270506be96ce239ea3ee4bf06
ismqui <ismqui.git@gmail.com>
2018-01-29 22:52:37 +0100

    add a supervisor for agent

--- lib/captacion_agua/application.ex
+++ lib/captacion_agua/application.ex
@@ -4,15 +4,25 @@ defmodule CaptacionAgua.Application do
   """
   use Application
 
-  alias CaptacionAgua.CacheData
-
   @doc """
   Function that start the app, loads data from a file into cache with agents
   and waits for clients to call it. The function is call when the app is loaded.
   """
   def start(_type, _args) do
     IO.puts "Starting Captacion Agua..."
-    {:ok, _pid} = CacheData.start_link()
+
+    import Supervisor.Spec
+
+    children = [
+      worker(CaptacionAgua.CacheData, []),
+    ]
+
+    options = [
+      name: CaptacionAgua.Supervisor,
+      strategy: :one_for_one,
+    ]
+
+    Supervisor.start_link(children, options)
   end
 
 end
